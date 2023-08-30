10f5d688d8a36defa46d14e216356b6ec07086d2
MegDacus <124758848+MegDacus@users.noreply.github.com>
2023-08-30 12:16:16 +1000

    Updated reducer to follow conventions

--- src/screens/ColorAdjusterScreen.js
+++ src/screens/ColorAdjusterScreen.js
@@ -1,39 +1,52 @@
-import React, {useState} from "react";
+import React, { useReducer } from "react";
 import { Pressable, Text, View, StyleSheet } from "react-native";
 import ColorCard from "../components/ColorCard";
 
 const COLOR_INCREMENT = 15
 
-const ColorAdjusterScreen = () => {
-    const [ red, setRed ] = useState(0);
-    const [ green, setGreen ] = useState(0);
-    const [ blue, setBlue ] = useState(0);
+const reducer = (state, action) => {
+    // state === { red: number, green: number, blue: number};
+    // action === { type: 'change_red' || 'change_green' || 'change_blue', payload: 15 || -15}
 
-    const setColor = (color, change) => {
-        // color === 'red, 'green', 'blue
-        // change === +15, -15
-
-        switch (color) {
-            case 'red': 
-                red + change > 255 || red + change < 0 ? null : setRed(red + change);
-                return;
-            case 'green':
-                green + change > 255 || green + change < 0 ? null : setGreen(green + change);
-                return;
-            case 'blue':
-                blue + change > 255 || blue + change < 0 ? null : setBlue(blue + change);
-                return;
-            default:
-                return;
-        }
+    switch (action.type) {
+        case 'change_red': 
+            return state.red + action.payload > 255 || state.red + action.payload < 0 
+                ? state
+                : { ...state, red: state.red + action.payload}
+        case 'change_green': 
+            return state.green + action.payload > 255 || state.green + action.payload < 0 
+                ? state
+                : { ...state, green: state.green + action.payload}
+        case 'change_blue':
+            return state.blue + action.payload > 255 || state.blue + action.payload < 0 
+                ? state
+                : { ...state, blue: state.blue + action.payload}
+        default:
+            return state;
     }
+}
+
+const ColorAdjusterScreen = () => {
+
+
+    const [state, dispatch] = useReducer(reducer, { red: 0, green: 0, blue: 0});
+    const { red, green, blue } = state;
 
     return (
         <View>
             <Text style={styles.text}>Color Adjuster</Text>
-            <ColorCard colorName="Red" onIncrease={() => setColor('red', COLOR_INCREMENT)} onDecrease={() => setColor('red', -1 * COLOR_INCREMENT)} />
-            <ColorCard colorName="Green" onIncrease={() => setColor('green', COLOR_INCREMENT)} onDecrease={() => setColor('green', -1 * COLOR_INCREMENT)}/>
-            <ColorCard colorName="Blue" onIncrease={() => setColor('blue', COLOR_INCREMENT)} onDecrease={() => setColor('blue', -1 * COLOR_INCREMENT)}/>
+            <ColorCard 
+                colorName="Red" 
+                onIncrease={() => dispatch({type: 'change_red', payload: COLOR_INCREMENT})} 
+                onDecrease={() => dispatch({type: 'change_red', payload: -1 * COLOR_INCREMENT})} />
+            <ColorCard 
+                colorName="Green" 
+                onIncrease={() => dispatch({type: 'change_green', payload: COLOR_INCREMENT})} 
+                onDecrease={() => dispatch({type: 'change_green', payload: -1 * COLOR_INCREMENT})} />
+            <ColorCard 
+                colorName="Blue" 
+                onIncrease={() => dispatch({type: 'change_blue', payload: COLOR_INCREMENT})} 
+                onDecrease={() => dispatch({type: 'change_blue', payload: -1 * COLOR_INCREMENT})} />
             <View style={{ alignSelf: 'center', height: 100, width: 100, backgroundColor: `rgb(${red}, ${green}, ${blue})`, marginTop: COLOR_INCREMENT }}/>
         </View>
     )
